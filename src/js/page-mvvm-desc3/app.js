2a06b0a2d7b493b6fffd80b811827fd18b56ec08
威晒 <masterkingr9@gmail.com>
2021-01-02 15:43:13 +0800

    update files

---
+++ src/js/page-mvvm-desc3/app.js
@@ -0,0 +1,23 @@
+/* global MVVM */
+
+;(function () {
+  'use strict'
+
+  new MVVM({
+    data: {
+      page: 1,
+      list: [],
+      keyword: null
+    },
+
+    methods: {
+      nextPage: function () {
+        
+      }
+    },
+
+    init: function () {
+      
+    }
+  })
+})()
