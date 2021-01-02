1d92d49e3b402e22f44aa4b87e12560facbc1c29
威晒 <masterkingr9@gmail.com>
2021-01-03 00:48:35 +0800

    update webpack

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -6,77 +6,80 @@ const TerserPlugin = require('terser-webpack-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const path = require('path')
 
-module.exports = (env, opts) => ({
-  context: path.resolve(__dirname, '../'),
-
-  entry: {
-    vendor: [
-      './src/css/reset.css',
-      './src/js/utils/helpers.js',
-      './src/css/taginput.css',
-      './src/js/components/tagInput.js',
-      './src/js/components/intersection-observer-polyfill.js',
-      './src/js/components/lazyLoad.js',
-    ],
-    desc3: [
-      './src/css/desc3.css',
-      './src/js/page-desc3/app.js',
-    ]
-  },
-
-  output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: opts.mode ? './js/[name].js' : './js/[name].[chunkhash].js'
-  },
-
-  devServer: {
-    port: 8081,
-    contentBase: path.resolve(__dirname, '../src'),
-    liveReload: true
-    // watchContentBase: true
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../src', 'tpl-desc3.html'),
-      chunks: ['vendor', 'desc3']
-    }),
-
-    new CopyPlugin({
-      patterns: [
-        {
-          from: './src/js/utils/mock.js',
-          to: 'js/utils/',
-        }
+module.exports = (env, opts) => {
+  console.log('opts: ', opts.mode);
+  return {
+    context: path.resolve(__dirname, '../'),
+  
+    entry: {
+      vendor: [
+        './src/css/reset.css',
+        './src/js/utils/helpers.js',
+        './src/css/taginput.css',
+        './src/js/components/tagInput.js',
+        './src/js/components/intersection-observer-polyfill.js',
+        './src/js/components/lazyLoad.js',
       ],
-    }),
-
-    new MiniCssExtractPlugin({
-      filename: opts.mode ? './css/[name].css' : './css/[name].[chunkhash].css'
-    })
-  ],
-
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: 'img/[name].[hash:7].[ext]'
-        }
+      desc3: [
+        './src/css/desc3.css',
+        './src/js/page-desc3/app.js',
+      ]
+    },
+  
+    output: {
+      path: path.resolve(__dirname, '../dist'),
+      filename: opts.mode == 'development' ? './js/[name].js' : './js/[name].[chunkhash].js'
+    },
+  
+    devServer: {
+      port: 8081,
+      contentBase: path.resolve(__dirname, '../src'),
+      liveReload: true
+      // watchContentBase: true
+    },
+  
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, '../src', 'tpl-desc3.html'),
+        chunks: ['vendor', 'desc3']
+      }),
+  
+      new CopyPlugin({
+        patterns: [
+          {
+            from: './src/js/utils/mock.js',
+            to: 'js/utils/',
+          }
+        ],
+      }),
+  
+      new MiniCssExtractPlugin({
+        filename: opts.mode == 'development' ? './css/[name].css' : './css/[name].[chunkhash].css'
+      })
+    ],
+  
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader']
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: 'img/[name].[hash:7].[ext]'
+          }
+      }
+      ]
+    },
+  
+    optimization: {
+      minimizer: [
+        new TerserPlugin(),
+        new CssMinimizerPlugin()
+      ]
     }
-    ]
-  },
-
-  optimization: {
-    minimizer: [
-      new TerserPlugin(),
-      new CssMinimizerPlugin()
-    ]
   }
-})
+}
